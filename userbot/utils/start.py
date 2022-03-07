5bdefe0bd45562463f5d40442cfef8146ea6a6ef
izzafthni <99348047+izzafthni@users.noreply.github.com>
2022-03-07 09:17:20 +0700

    Update start.py

--- userbot/utils/start.py
+++ userbot/utils/start.py
@@ -12,7 +12,7 @@ async def startupmessage():
             await tgbot.send_file(
                 BOTLOG_CHATID,
                 "https://telegra.ph/file/02f87cca391f9b9d627d5.jpg",
-                caption="✨ **Kyy Userbot Has Been Actived**!!\n━━━━━━━━━━━━━━━\n➠ **Userbot Version** - 8.0@master\n━━━━━━━━━━━━━━━\n➠ **Powered By:** @NastyProject ",
+                caption="✨ **Ezz-bot Has Been Actived**!!\n━━━━━━━━━━━━━━━\n➠ **Userbot Version** - 8.0@master\n━━━━━━━━━━━━━━━\n➠ **Powered By:** @NastyProject ",
                 buttons=[(Button.url("ꜱᴜᴘᴘᴏʀᴛ", "https://t.me/NastySupportt"),)],
             )
     except Exception as e:
